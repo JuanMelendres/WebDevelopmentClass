c5b94b35fab1e1a3bb8caffbbc1bb2f98ccd6e91
JuanMelendres <juan.mele97@gmail.com>
2021-02-23 21:16:27 -0600

    Finish Activity 1 & 2
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- ajax/ajax02.go
+++ ajax/ajax02.go
@@ -1,41 +1,64 @@
 package main
 
 import (
- //"fmt"
-  "net/http"
-  "encoding/json"
-
+	"encoding/json"
+	"net/http"
+	"strconv"
 )
 
+var a = [5]int{2, 4, 6, 8, 10}
 
 type book struct {
 	Titulo string `json: titulo`
-	Autor  string    `json: autor`
+	Autor  string `json: autor`
+}
+
+type user struct {
+	UserId string `json: userId`
+	ID  string `json: id`
+  Title  string `json: title`
 }
 
 func mostrarHTML(w http.ResponseWriter, r *http.Request) {
-   http.ServeFile(w, r, "ajax02.html")
+	http.ServeFile(w, r, "ajax02.html")
 }
 
 func darMensaje(w http.ResponseWriter, r *http.Request) {
-	//r.ParseForm() 
-	//x := r.Form.Get("y")
-  //fmt.Printf("%s", x)
-  libro := book{
-    Titulo: "La Casa",
-    Autor:  "Paco Roca",
+	if r.Method == "GET" {
+		json.NewEncoder(w).Encode(a)
+	}
+	if r.Method == "POST" {
+		r.ParseForm()
+		x := r.Form.Get("y")
+		i1, err := strconv.ParseInt(x, 10, 64)
+		if err == nil {
+			json.NewEncoder(w).Encode(a[i1])
+		}
+	}
+}
+
+func darMensaje2(w http.ResponseWriter, r *http.Request) {
+	// libro := book{
+	// 	Titulo: "La Casa",
+	// 	Autor:  "Paco Roca",
+	// }
+  data := user {
+    UserId: "1",
+    ID: "1",
+    Title: "delectus aut autem",
   }
-  //w.Header().Set("Content-Type", "application/json") 
-  json.NewEncoder(w).Encode(libro)
+	// w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
 
 func main() {
-	
-  http.HandleFunc("/dato", darMensaje)
-  http.HandleFunc("/",mostrarHTML)
-  
-  err := http.ListenAndServe("localhost"+":"+"8080", nil)
-  if err != nil {
-    return
-  }
+
+	http.HandleFunc("/dato", darMensaje)
+  http.HandleFunc("/json", darMensaje2)
+	http.HandleFunc("/", mostrarHTML)
+
+	err := http.ListenAndServe("localhost"+":"+"8080", nil)
+	if err != nil {
+		return
+	}
 }
