9b792f86e48b2cc2853a52fa9186e292d31c24f2
JuanMelendres <juan.mele97@gmail.com>
2021-02-23 19:30:09 -0600

    Add JQuery Data and Finish Activity
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- ajax/activiad3.go
+++ ajax/activiad3.go
@@ -32,3 +32,17 @@ func ServeTestJson(w http.ResponseWriter, r *http.Request) {
 	log.Printf("request testjson: %s", r.Host)
 	http.ServeFile(w, r, "data.json")
 }
+
+// JQuery
+// es una biblioteca multiplataforma de JavaScript, creada inicialmente por John Resig,
+// que permite simplificar la manera de interactuar con los documentos HTML, manipular el árbol DOM, manejar eventos,
+// desarrollar animaciones y agregar interacción con la técnica AJAX a páginas web.
+
+// Ventajas
+// Nos permite realizar consultas sobre la estructura del DOM de la web y realizar personalizaciones a medida como
+// aplicar diferentes estilos y efectos en función de los eventos que definamos.
+
+// Desventajas
+// La gran cantidad de versiones publicadas
+// No importa si usted está corriendo la última versión de jQuery, usted tendrá que hostear la librería usted mismo,
+//  o descargar la librería desde Google
