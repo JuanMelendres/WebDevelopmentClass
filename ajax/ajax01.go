c00ccb8ee7476382eb015143a79bd55dc13726f5
JuanMelendres <juan.mele97@gmail.com>
2021-02-23 20:05:18 -0600

    Update request data
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- ajax/ajax01.go
+++ ajax/ajax01.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-  "fmt"
-  
-  "net/http"
+	"fmt"
 
+	"net/http"
 )
 
 func darMensaje(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World, %s! \n'", r)
+	// fmt.Fprintf(w, "Hello World, %s! \n", r)
+	// fmt.Fprintf(w, "User info, %s! \n", r.UserAgent())
+	// fmt.Fprintf(w, "Method, %s! \n", r.Method)
+	// fmt.Fprintf(w, "url, %s! \n", r.URL)
+  // fmt.Fprintf(w, "Host, %s! \n", r.Host) 
+  // fmt.Fprintf(w, "Cookies, %s! \n", r.Cookies()) Location
+  fmt.Fprintf(w, "RemoteAddr, %s! \n", r.RemoteAddr)
 }
 
 func main() {
-  http.HandleFunc("/", darMensaje)
-  err := http.ListenAndServe("localhost"+":"+"8080", nil)
-  if err != nil {
-    return
-  }
+	http.HandleFunc("/", darMensaje)
+	err := http.ListenAndServe("localhost"+":"+"8080", nil)
+	if err != nil {
+		return
+	}
 }
