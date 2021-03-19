3774b8e4a75c5e376b86a92c517a7e712772c245
JuanMelendres <juan.mele97@gmail.com>
2021-03-19 17:07:33 -0600

    Add bootstrap
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- ajax/vue01.go
+++ ajax/vue01.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Pelicula struct {
-	Name string `json:"name"`
-	Year int `json:"year"`
+	Name     string `json:"name"`
+	Year     int    `json:"year"`
 	Director string `json:"director"`
-	Rating int `json:"rating"`
+	Rating   int    `json:"rating"`
+	Image    string `json:"url"`
 }
 
 func mostrarHTML(w http.ResponseWriter, r *http.Request) {
@@ -20,28 +21,32 @@ func mostrarHTML(w http.ResponseWriter, r *http.Request) {
 func peliculas(w http.ResponseWriter, r *http.Request) {
 	peliculas := []Pelicula{
 		{
-			Name: "The Avengers",
-			Year: 2012,
+			Name:     "The Avengers",
+			Year:     2012,
 			Director: "Joss Whedon",
-			Rating: 5,
+			Rating:   5,
+			Image:    "https://generacionxbox.com/wp-content/uploads/2019/05/avengers-xboxone-marvel-generacion-xbox-940x529.jpg",
 		},
 		{
-			Name: "Star Wars: Episode III",
-			Year: 2005,
+			Name:     "Star Wars: Episode III",
+			Year:     2005,
 			Director: "George Lucas",
-			Rating: 5,
+			Rating:   5,
+			Image:    "https://static.wikia.nocookie.net/cine/images/2/23/Star-Wars-Episode-III-Revenge-of-the-Sith.jpg/revision/latest/top-crop/width/360/height/450?cb=20120930201011",
 		},
 		{
-			Name: "The Lord of the Rings: The Return of the King",
-			Year: 2003,
+			Name:     "The Lord of the Rings: The Return of the King",
+			Year:     2003,
 			Director: "Peter Jackson",
-			Rating: 5,
+			Rating:   5,
+			Image:    "https://upload.wikimedia.org/wikipedia/en/b/be/The_Lord_of_the_Rings_-_The_Return_of_the_King_%282003%29.jpg",
 		},
 		{
-			Name: "The Fast and the Furious",
-			Year: 2001,
+			Name:     "The Fast and the Furious",
+			Year:     2001,
 			Director: "Rob Cohen",
-			Rating: 5,
+			Rating:   5,
+			Image:    "https://static.wikia.nocookie.net/doblaje/images/4/47/RFCartelOriginal.jpg/revision/latest?cb=20171125061739&path-prefix=es",
 		},
 	}
 	w.Header().Set("Content-Type", "application/json")
